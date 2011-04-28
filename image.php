50909e5edfa7836470fcc9e9b17bdf856f2417d6
Neil Crosby <neil@thetenwordreview.com>
2011-04-28 23:13:31 +0100

    Hint to the browser what to call the image

--- image.php
+++ image.php
@@ -4,5 +4,8 @@ require_once('Turtle.php');
 
 $turtle = new Turtle($_GET['commands'], 200, 200);
 
+$filename = str_replace(' ', '-', $_GET['commands']);
+
+header ("Content-Disposition: Attachment;filename=$filename.png"); 
 header ('Content-Type: image/png');
 echo $turtle->getImage();
